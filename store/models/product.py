1645de6eb007a6991f0eeb3a270bb7ffe8f1d1a8
Chris4891 <kolar.kristijan84@gmail.com>
2024-01-07 13:34:46 +0500

    Added a function to get all the products by category id

--- store/models/product.py
+++ store/models/product.py
@@ -14,3 +14,9 @@ class Products(models.Model):
     def get_all_products():
         return Products.objects.all()
 
+    @staticmethod
+    def get_all_products_by_categoryid(category_id):
+        if category_id:
+            return Products.objects.filter (category=category_id)
+        else:
+            return Products.get_all_products()
