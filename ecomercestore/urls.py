f32c80777aa78b2c0fcdfdcd3a6bd99d380cc2ad
Chris4891 <kolar.kristijan84@gmail.com>
2024-01-07 13:09:07 +0500

    Added urls to admin.site

--- ecomercestore/urls.py
+++ ecomercestore/urls.py
@@ -7,3 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('' , include('store.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+admin.site.site_header = "Gear Up & Glow Admin"
+admin.site.site_title = "Gear Up & Glow Admin Portal"
+admin.site.index_title = "Welcome to Gear Up & Glow Admin Portal"
+admin.site.unregister(Group)
