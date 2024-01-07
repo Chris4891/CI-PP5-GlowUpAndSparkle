e30555708ba5456d797e531fe69758b2a9ac50e9
Chris4891 <kolar.kristijan84@gmail.com>
2024-01-07 13:22:27 +0500

    Added the orders table/model

--- store/migrations/0001_initial.py
+++ store/migrations/0001_initial.py
@@ -42,5 +42,18 @@ class Migration(migrations.Migration):
                 ('category', models.ForeignKey(default=1, on_delete=django.db.models.deletion.CASCADE, to='store.category')),
             ],
         ),
-
+        migrations.CreateModel(
+            name='Order',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('quantity', models.IntegerField(default=1)),
+                ('price', models.IntegerField()),
+                ('address', models.CharField(blank=True, default='', max_length=50)),
+                ('phone', models.CharField(blank=True, default='', max_length=50)),
+                ('date', models.DateField(default=datetime.datetime.today)),
+                ('status', models.BooleanField(default=False)),
+                ('customer', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='store.customer')),
+                ('product', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='store.products')),
+            ],
+        ),
     ]
