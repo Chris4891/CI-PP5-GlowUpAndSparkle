d8129725f41535f03fa4432355c6b921204aa71c
Chris4891 <109537705+Chris4891@users.noreply.github.com>
2024-01-10 22:17:51 +0500

    Update cart.py to place order without login as customer

--- store/views/cart.py
+++ store/views/cart.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render , redirect
 from django.http import HttpResponse
-
+from store.models.product import Products
 from django.contrib.auth.hashers import  check_password
 from store.models.customer import Customer
 from django.views import  View
 from store.models.product import Products
+from django.contrib import messages
+
 
 class Cart(View):
     def get(self, request):
@@ -17,3 +19,29 @@ class Cart(View):
             products = []  # Define an empty list when the cart is empty
 
         return render(request, 'cart.html', {'products': products})
+    
+    def post(self , request):
+        product = request.POST.get('product')
+        remove = request.POST.get('remove')
+        cart = request.session.get('cart')
+        if cart:
+            quantity = cart.get(product)
+            if quantity:
+                if remove:
+                    if quantity<=1:
+                        cart.pop(product)
+                    else:
+                        cart[product]  = quantity-1
+                else:
+                    cart[product]  = quantity+1
+
+            else:
+                cart[product] = 1
+        else:
+            cart = {}
+            cart[product] = 1
+
+        request.session['cart'] = cart
+        print('cart' , request.session['cart'])
+
+        return redirect('cart')
