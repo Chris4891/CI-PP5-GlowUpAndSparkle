ddcb5ecce3535754a3add1aefda106be55406b00
Chris4891 <109537705+Chris4891@users.noreply.github.com>
2024-01-10 22:25:08 +0500

    Update urls.py to place order without login as customer

--- store/urls.py
+++ store/urls.py
@@ -4,14 +4,13 @@ from .views.home import Index
 from .views.signup import Signup
 from .views.login import Login , logout
 from .views.cart import Cart
-# from .views.checkout import checkout
+from .views.checkout import create_checkout_session, success, cancel,orderdetails_guest,orderdetails
 from .views.orders import OrderView
 from .middlewares.auth import  auth_middleware
 from .views.store import store
 from .views.contact import  contact_page,response
 from .views.about import About
 from .views.ppolicy import ppolicy
-from .views.checkout import create_checkout_session, success, cancel,orderdetails
 from .views.newsletter import Newsletter
 from .views.productdetail import ProductDetailView
 from django.conf import settings
